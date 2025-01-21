16031e6390c9cf39318495e31862849eee9bcaeb
Avunu LLC <mail@avu.nu>
2025-01-21 11:05:16 -0500

    fix: contact update hook

--- jwt_auth/jwt_auth/hooks/contact.py
+++ jwt_auth/jwt_auth/hooks/contact.py
@@ -1,12 +1,36 @@
 import frappe
 
+
 def on_update(doc, method):
-	# update the user's name(s) if the contact's name is updated
-	if not doc.user:
-		return
-	if doc.has_value_changed("first_name"):
-		frappe.db.set_value("User", doc.user, "first_name", doc.first_name)
-	if doc.has_value_changed("middle_name"):
-		frappe.db.set_value("User", doc.user, "middle_name", doc.middle_name)
-	if doc.has_value_changed("last_name"):
-		frappe.db.set_value("User", doc.user, "last_name", doc.last_name)
+
+    if "[Change Me]" in doc.name and doc.full_name:
+        frappe.enqueue(
+            "frappe.model.rename_doc.rename_doc",
+            doctype=doc.doctype,
+            old=doc.name,
+            new=doc.full_name,
+            force=False,
+            show_alert=True,
+        )
+
+    if doc.user:
+        # Update user's fields from contact
+        user_fields = {
+            "first_name": doc.first_name,
+            "middle_name": doc.middle_name,
+            "last_name": doc.last_name,
+            "full_name": doc.full_name,
+            "phone": doc.phone,
+            "mobile_no": doc.mobile_no,
+            "gender": doc.gender,
+        }
+
+        # Only update fields that have changed
+        update_fields = {
+            field: value
+            for field, value in user_fields.items()
+            if doc.has_value_changed(field) and value is not None
+        }
+
+        if update_fields:
+            frappe.db.set_value("User", doc.user, update_fields)
